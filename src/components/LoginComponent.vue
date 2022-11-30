1d51a8ad70e5ebb4238fdb3d602f56d2382d76b9
Lucas Erthal <57104379+LucasErthal@users.noreply.github.com>
2022-11-30 15:06:19 -0300

    feat: create LoginComponent base

---
+++ src/components/LoginComponent.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="login-component-container">
+    <div class="input-container">
+      <label for="user-input" class="label">User</label>
+      <input type="text" id="user-input" class="input" />
+    </div>
+
+    <div class="input-container">
+      <label for="password-input" class="label">Password</label>
+      <input type="text" id="password-input" class="input" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "LoginComponent",
+
+  data() {
+    return {};
+  },
+});
+</script>
+
+<style scoped>
+#login-component-container {
+  width: 600px;
+  height: 300px;
+  display: flex;
+  flex-direction: column;
+  border-radius: 20px;
+
+  align-items: center;
+  justify-content: center;
+  background-color: rgb(170, 8, 170);
+}
+
+.input-container {
+  display: flex;
+  flex-direction: column;
+
+  width: 50%;
+}
+
+.input-container:not(:last-child) {
+  margin: 0 0 32px 0;
+}
+
+.input-container label {
+  color: white;
+  margin: 0 0 4px 0;
+}
+
+.input-container input {
+  border-radius: 12px;
+  height: 40px;
+  border: 0;
+  padding: 0 8px;
+  box-shadow: 8px 8px 8px rgba(0, 0, 0, 0.2);
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,28 @@
-<script lang="ts">
-import { defineComponent } from "vue";
-
-export default defineComponent({});
-</script>
-
 <template>
-  <div>
-    <span>Vue.js template</span>
+  <div id="home-view-container">
+    <login-component />
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import LoginComponent from "../components/LoginComponent.vue";
+
+export default defineComponent({
+  components: {
+    LoginComponent,
+  },
+});
+</script>
+
+<style scoped>
+#home-view-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  height: 100%;
+}
+</style>
