f6e889df3805cf4bb366a16fabb539707d780b15
Lucas Erthal <57104379+LucasErthal@users.noreply.github.com>
2022-12-06 14:36:13 -0300

    test: e2e tests refactor

--- cypress/e2e/example.cy.ts
+++ cypress/e2e/example.cy.ts
@@ -1,8 +1,26 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe("My First Test", () => {
+describe("Example Test", () => {
+  const user = "Tester_User";
+
   it("visits the app root url", () => {
     cy.visit("/");
-    cy.contains("h1", "You did it!");
+  });
+
+  it("should display LoginComponent", () => {
+    cy.get("#login-component-container").should("be.visible");
+  });
+
+  it("should pass user name as route prop", () => {
+    cy.get("#user-input").type(user, { delay: 50 });
+    cy.get("#password-input").type("123123", { delay: 50 });
+
+    cy.get(".login-button").click();
+
+    cy.url().should("include", user);
+  });
+
+  it("should show the route prop on LoginConfirmationComponent", () => {
+    cy.get("#login-confirmation-component-container").contains(user);
   });
 });
