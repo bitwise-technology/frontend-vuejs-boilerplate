111bba15cbf137c215e6a24b6debd7ffd3d0fa59
Lucas Erthal <57104379+LucasErthal@users.noreply.github.com>
2022-12-18 20:15:10 -0300

    chore: create setAuthResponseAction and authWithEmailAndPasswordAction

--- src/stores/userStore.ts
+++ src/stores/userStore.ts
@@ -1,9 +1,11 @@
 import { defineStore } from "pinia";
-import type { User } from "@/types";
+import { api } from "@/services/api";
+import type { AuthRequest, AuthResponse, User } from "@/types";
 
 export const userStore = defineStore("user", {
   state: () => ({
     user: {} as User,
+    authResponse: {} as AuthResponse,
   }),
   getters: {
     userGetter: (state) => state.user,
@@ -11,6 +13,23 @@ export const userStore = defineStore("user", {
   actions: {
     setUserAction(user: User) {
       this.user = user;
+    },
+
+    setAuthResponseAction(authResponse: AuthResponse) {
+      this.authResponse = authResponse;
+    },
+
+    async authWithEmailAndPasswordAction(
+      authRequest: AuthRequest
+    ): Promise<AuthResponse> {
+      const response = await api.post(
+        "/accounts:signInWithPassword",
+        authRequest
+      );
+
+      //this.setAuthResponseAction(response.data);
+
+      return response.data;
     },
   },
 });
