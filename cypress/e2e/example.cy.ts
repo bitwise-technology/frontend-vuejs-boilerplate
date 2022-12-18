a23752bbc71978471bac40729fac5293653379b8
Lucas Erthal <57104379+LucasErthal@users.noreply.github.com>
2022-12-18 20:26:12 -0300

    test: change valid_user to a registered email

--- cypress/e2e/example.cy.ts
+++ cypress/e2e/example.cy.ts
@@ -1,7 +1,8 @@
 // https://docs.cypress.io/api/introduction/api.html
 
 describe("Example Test", { testIsolation: false }, () => {
-  const user = "Tester_User";
+  const email = "teste@mail.com";
+  const password = "123123";
 
   it("visits the app root url", () => {
     cy.visit("/");
@@ -11,16 +12,16 @@ describe("Example Test", { testIsolation: false }, () => {
     cy.get("#login-component-container").should("be.visible");
   });
 
-  it("should pass user name as route prop", () => {
-    cy.get("#user-input").type(user, { delay: 50 });
-    cy.get("#password-input").type("123123", { delay: 50 });
+  it("should pass user email as route prop", () => {
+    cy.get("#user-input").type(email, { delay: 50 });
+    cy.get("#password-input").type(password, { delay: 50 });
 
     cy.get(".login-button").click();
 
-    cy.url().should("include", user);
+    cy.url().should("include", email);
   });
 
   it("should show the route prop on LoginConfirmationComponent", () => {
-    cy.get("#login-confirmation-component-container").contains(user);
+    cy.get("#login-confirmation-component-container").contains(email);
   });
 });
