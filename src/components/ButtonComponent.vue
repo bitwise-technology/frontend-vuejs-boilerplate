13b6976ba7e2f2ef58544f1ef8de3ac4994ef8d8
Lucas Erthal <57104379+LucasErthal@users.noreply.github.com>
2022-12-27 10:21:25 -0300

    feat: create ButtonComponent

---
+++ src/components/ButtonComponent.vue
@@ -0,0 +1,49 @@
+<template>
+  <button @click="onPress" :style="`background-color: ${bgColor}`">
+    <span :style="`color: ${textColor}`">{{ text }}</span>
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ButtonComponent",
+  props: {
+    bgColor: {
+      type: String,
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+    textColor: {
+      type: String,
+      required: true,
+    },
+    onPress: {
+      type: Function,
+    },
+  },
+});
+</script>
+
+<style scoped>
+button {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 60px;
+  min-width: 120px;
+  border: none;
+  border-radius: 8px;
+  padding: 0 16px;
+  cursor: pointer;
+  box-shadow: 4px 4px 10px;
+}
+
+span {
+  font-size: 16px;
+}
+</style>
