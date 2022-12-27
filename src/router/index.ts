1191cd23ea0f46b37ff83bd308612945a4887b99
Lucas Erthal <57104379+LucasErthal@users.noreply.github.com>
2022-12-27 10:20:28 -0300

    chore: add HomeView to Routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 
+import HomeView from "../views/HomeView.vue";
 import LoginView from "../views/LoginView.vue";
-import LoginConfirmationView from "@/views/LoginConfirmationView.vue";
+import LoginConfirmationView from "../views/LoginConfirmationView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomeView,
+    },
     {
       path: "/login",
       name: "login",
