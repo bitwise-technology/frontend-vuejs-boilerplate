8b6e9a763e3d4d09b8e59f2dddc73e349df15eeb
Lucas Erthal <57104379+LucasErthal@users.noreply.github.com>
2022-12-28 18:06:44 -0300

    test: remove route prop verification on login flux in e2e tests

--- cypress/e2e/example.cy.ts
+++ cypress/e2e/example.cy.ts
@@ -1,5 +1,4 @@
 // https://docs.cypress.io/api/introduction/api.html
-
 describe("Example Test", { testIsolation: false }, () => {
   const email = "teste@mail.com";
   const password = "123123";
@@ -18,16 +17,16 @@ describe("Example Test", { testIsolation: false }, () => {
     cy.get("#login-component-container").should("be.visible");
   });
 
-  it("should pass user email as route prop", () => {
+  it("should save user state and navigate to login-confirmation", () => {
     cy.get("#user-input").type(email, { delay: 50 });
     cy.get("#password-input").type(password, { delay: 50 });
 
-    cy.get(".login-button").click();
+    cy.get("#login-button").click();
 
-    cy.url().should("include", email);
+    cy.location("pathname").should("match", /\/login-confirmation$/);
   });
 
-  it("should show the route prop on LoginConfirmationComponent", () => {
+  it("should show email on LoginConfirmationComponent", () => {
     cy.get("#login-confirmation-component-container").contains(email);
   });
 });
